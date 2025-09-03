f3f84e2f47e7be66ff23e3f7ddbd432347f73cfe
skylar <2031499619@qq.com>
2025-09-03 15:20:34 +0800

    feat: change card name

--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,26 @@
 // Convert to title
-export const parseTitle = (currentPage: string) => {
-  const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
-  let title = '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
-  if (title.slice(-1) == '/') {
-    title = title.substring(0, title.length - 1);
-  }
-  return title
-}
+export const parseTitle = (currentPage: string, frontMatterTitle?: string) => {
+	const [indexPart, defaultTitle] = decodeURIComponent(currentPage.split('/posts/')[1]).split('-');
+	// const baseTitle = `第${indexPart}期 - `;
+	const titleSuffix = frontMatterTitle ? `${frontMatterTitle}/` : defaultTitle;
+	let title = titleSuffix;
+
+	// 移除末尾的斜杠
+	if (title.endsWith('/')) {
+		title = title.slice(0, -1);
+	}
+	return title;
+};
 
 // Get the current article number.
 export const getIndex = (currentPage: string) => {
-  const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
-  return parseInt(oldTitle.split('-')[0])
-}
+	const oldTitle = decodeURIComponent(currentPage.split('/posts/')[1]);
+	return parseInt(oldTitle.split('-')[0]);
+};
 
 // Sort all articles.
 export const sortPosts = (allPosts: any) => {
-  return allPosts.sort((a, b) => {
-    return (
-      parseInt(b.url.split('/posts/')[1].split('-')[0]) -
-      parseInt(a.url.split('/posts/')[1].split('-')[0])
-    );
-  });
-}
+	return allPosts.sort((a, b) => {
+		return parseInt(b.url.split('/posts/')[1].split('-')[0]) - parseInt(a.url.split('/posts/')[1].split('-')[0]);
+	});
+};
